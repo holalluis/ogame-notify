85d81004a8861f033f5a08e660e3fc2c95a0766e
holalluis <holalluis@gmail.com>
2019-05-28 23:06:33 +0200

    update

--- productivitat.py
+++ productivitat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import ogame as o
 '''
   CALCUL PRODUCTIVITAT: TEMPS DE VOL, CONSUM COMBUSTIBLE I BENEFICIS
   farming: sondes vs naus de càrrega
@@ -8,21 +7,21 @@ import ogame as o
   TODO: calcular amortitzacio
   TODO: equivalencia naus petites grans o espionatge
 '''
-kkk       = 446  #recursos (k) al planeta
-s1        = 438  #sistema sortida
-s2        = 414  #sistema arribada
-o.motor_c = 10   #motor combustión (velocitat)
-o.motor_i = 7    #motor impulso (velocitat)
-o.tec_hip = 7    #tecnologia hiperespacio (càrrega)
-boti = kkk*1e3/2 #botí màxim (50%)
+import ogame as o
+kkk       = 200       # recursos (k) al planeta
+s1        = 438       # sistema sortida
+s2        = 414       # sistema arribada
+o.motor_c = 14        # motor combustión (velocitat)
+o.motor_i = 9         # motor impulso (velocitat)
+o.tec_hip = 10        # tecnologia hiperespacio (càrrega)
+boti      = kkk*1e3/2 # botí màxim (50%)
 
 #flotes per simular
 flotes=[
   o.NausPC(),
-  #o.Sondes(), #o.NausGC(2),
+  o.Sondes(600),
+  #o.NausGC(2),
 ]
 
-botins=[60, 100, 120, 150, 180, 200]
-
 #simulacions
-for f in flotes: f.viatge(s1,s2,boti) 
+for f in flotes: f.viatge(s1,s2,boti)
